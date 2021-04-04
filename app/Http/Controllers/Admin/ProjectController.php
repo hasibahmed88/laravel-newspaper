0e806615a036fff26dea7d99cd35afa3ee78d431
Hasib Ahmed <hasibahmed885@gmail.com>
2021-04-04 14:42:25 +0600

    update admin dashboard

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Contact;
+use App\Models\Admin\News;
+use App\Models\Front\Comment;
+use App\Models\Front\Newslatter;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -24,6 +28,12 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        return view('admin.home.home');
+
+        $post = News::all();
+        $newslatter = Newslatter::all();
+        $message = Contact::where('status','0')->get();
+        $comments = Comment::where('status',0)->get();
+        
+        return view('admin.home.home',compact('post','newslatter','message','comments'));
     }
 }
